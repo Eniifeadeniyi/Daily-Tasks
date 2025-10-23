d97211f4a4e01ac24f87ddd80d8581ba3fbcbd48
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-10-23 06:00:23 +0100

    Add Breakfast.java

---
+++ Breakfast.java
@@ -0,0 +1,10 @@
+public class Breakfast {
+	public String getSentence(String sentence) {
+		return sentence;
+	}
+
+	public String convert(String sentence) {
+		return sentence.toUpperCase();
+
+	}
+}
---
+++ BreakfastTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class BreakfastTest {
+
+	@Test
+	public void testThatASentenceIsCollected() {	
+		Breakfast breakfast = new Breakfast();			
+		String result = breakfast.getSentence("God is good.");		
+		assertEquals(result, "God is good.");				
+
+	}
+
+	@Test
+	public void testThatASentenceIsCollectedAndConvertedToUpperCase() {	
+		Breakfast breakfast = new Breakfast();			
+		String result = breakfast.convert("God is good.");		
+		assertEquals(result, "GOD IS GOOD.");				
+
+	}
+
+}
+
+
