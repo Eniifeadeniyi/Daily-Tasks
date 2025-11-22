ef5ee414e7c8a7caf08bc9f032331d027fd2e1f2
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-11-22 13:26:06 +0100

    ...

--- DayElevenBreakfast.js
+++ DayElevenBreakfast.js
@@ -1,14 +1,21 @@
+function validate(input) {
+    let check = []
+    let valid = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " "]
+    for(let num of input) {
+        if(valid.includes(num)) check.push(true)
+        else check.push(false);}
+    if(check.includes(false))return false;
+    else return true;
+}
 let prompt = require('prompt-sync')(); 
-let input = prompt('Enter numbers spaced by one space: ')
+let input = prompt('Enter numbers spaced by one space: ').trim();
+while(!(validate(input))) {
+    console.log("Invalid input!")
+    input = prompt('Enter numbers spaced by one space: ');
+}
+
 let numbers = input.split(" ")
-
-let valid = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-let validInput = numbers.filter((number) => number in valid)
-
-let nums = validInput.map((number) => Number(number));
-console.log(validInput)
-
-
+let nums = numbers.map((number) => Number(number));
 
 if(nums.length > 1) {
 let max = nums[0];
