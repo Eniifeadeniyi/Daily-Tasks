a2cf02841ea988428608d6ab441c61db6c2f0d9b
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-11-22 13:07:23 +0100

    ...

--- DayTwelveBreakfast.js
+++ DayTwelveBreakfast.js
@@ -5,7 +5,7 @@ function makeArray(number){
     }
     return numbers;
 }
-function bubblSort(numbers) {
+function bubbleSort(numbers) {
     for(let count = 0; count < numbers.length; count++){
         for(let counter = 0; counter < numbers.length; counter++){
             if(numbers[count] < numbers[counter]){
@@ -19,4 +19,4 @@ function bubblSort(numbers) {
     return numbers.join("")
 }
 let numbers = makeArray("54145")
-console.log(bubblSort(numbers))
+console.log(bubbleSort(numbers))
