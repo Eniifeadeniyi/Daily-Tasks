d6050b3c8df866cc85f84dacbae98e9879239938
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-11-22 13:21:42 +0100

    ...

--- DayTwelveBreakfast.js
+++ DayTwelveBreakfast.js
@@ -8,7 +8,7 @@ function makeArray(number){
 function bubbleSort(numbers) {
     for(let count = 0; count < numbers.length; count++){
         for(let counter = 0; counter < numbers.length; counter++){
-            if(numbers[count] < numbers[counter]){
+            if(numbers[count] > numbers[counter]){
                 let temp = numbers[count];
                 numbers[count] = numbers[counter];
                 numbers[counter] = temp;
@@ -18,5 +18,23 @@ function bubbleSort(numbers) {
     }
     return numbers.join("")
 }
-let numbers = makeArray("54145")
-console.log(bubbleSort(numbers))
+
+function validate(input) {
+    let check = []
+    let valid = [0,1,2,3,4,5,6,7,8,9]
+    for(let num of input) {
+        if(valid.includes(Number(num))) check.push(true)
+        else check.push(false);}
+    if(check.includes(false))return false;
+    else return true;
+}
+const prompt = require('prompt-sync')();
+
+let input = prompt("Enter a number: ");
+
+while(!(validate(input))){
+    console.log("Invalid input!");
+    input = prompt("Enter a number: ");}
+
+let numbers = makeArray(input);
+console.log(bubbleSort(numbers));
