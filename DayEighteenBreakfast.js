e660fd1ee43e81de77d91e6fc3ce08ab344bc345
eniife <eniifeoluwaadeniyi16@gmail.com>
2025-11-27 10:57:41 +0100

    day 18

---
+++ DayEighteenBreakfast.js
@@ -0,0 +1,10 @@
+function inverse(numbers){
+    let output = []
+    for(let number of numbers){
+        if(number > 0) output.push(number - number * 2);
+        if(number < 0) output.push(number - number * 2);
+    }
+    return output;
+}
+
+console.log(inverse([1,-2,3,-4,5]))
